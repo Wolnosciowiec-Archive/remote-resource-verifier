9c4e2c7513b53237bb85425d47336cad76a6168b
Black and Red <wesoly.krzysztofa@gmail.com>
2016-11-15 07:22:39 +0100

    Added possibility to easily override settings by creating custom files and by setting environment variables

--- src/settings_prod.php
+++ src/settings_prod.php
@@ -1,15 +1,15 @@
 <?php
 
-return [
+$settings = [
     'settings' => [
         'debug'    => false,
-        'apiToken' => 'this-is-an-example-of-api-token',
+        'apiToken' => (getenv('WUV_TOKEN') ? getenv('WUV_TOKEN') : 'this-is-an-example-of-api-token'),
 
         // process the resource only once, keep it in history to not allow adding it again
-        'processOnlyOnce' => false,
+        'processOnlyOnce' => (getenv('WUV_PROCESS_ONLY_ONCE') ? (bool)getenv('WUV_PROCESS_ONLY_ONCE') : false),
 
         // limit to only X number of checks per one worker iteration eg. 16 checks per minute/request
-        'processing.limitation' => 16,
+        'processing.limitation' => (getenv('WUV_PROCESS_LIMITATION') ? (int)getenv('WUV_PROCESS_LIMITATION') : 16),
 
         'displayErrorDetails'    => false, // set to false in production
         'addContentLengthHeader' => true, // Allow the web server to send the content-length header
@@ -27,3 +27,9 @@ return [
         ],
     ],
 ];
+
+if (is_file(__DIR__ . '/settings_prod.custom.php')) {
+    $settings = array_merge($settings, require __DIR__ . '/settings_prod.custom.php');
+}
+
+return $settings;
